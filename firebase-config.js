b1a53f8ccc37c1b98e9fc79217f3d040a126bb1a
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-01-10 22:01:16 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
-// Configuração do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
+
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAO3As6jMMmENtzaK9zlDADbpS9UlNxx8o",
   authDomain: "mac-projeto-4e552.firebaseapp.com",
@@ -10,11 +8,10 @@ const firebaseConfig = {
   storageBucket: "mac-projeto-4e552.appspot.com",
   messagingSenderId: "537330451519",
   appId: "1:537330451519:web:5a1b4c921119b5ee98e48a",
-  measurementId: "G-XXXXXXXXXX"
 };
 
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 

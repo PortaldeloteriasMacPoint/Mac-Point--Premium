65a084b8faded56133f821386872b8cd62ffdfa9
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-04-23 22:50:08 -0300

    Create modalidades.js

---
+++ modalidades.js
@@ -0,0 +1,46 @@
+var loterias = [
+  {
+    nome: "Mega-Sena",
+    icone: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Mega-Sena_logo.png",
+    dezenas: ["05", "12", "23", "34", "45", "56"],
+    proximo: "27/04/2025",
+    status: "Acumulado: R$ 48 milhões"
+  },
+  {
+    nome: "Quina",
+    icone: "https://upload.wikimedia.org/wikipedia/pt/thumb/2/2c/Logo_Quina_caixa.png/320px-Logo_Quina_caixa.png",
+    dezenas: ["08", "19", "27", "38", "44"],
+    proximo: "25/04/2025",
+    status: "Teve ganhador"
+  },
+  {
+    nome: "Lotofácil",
+    icone: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/3d/Lotof%C3%A1cil_logo.png/320px-Lotof%C3%A1cil_logo.png",
+    dezenas: ["01", "03", "04", "06", "07", "08", "09", "10", "11", "13", "14", "16", "18", "20", "25"],
+    proximo: "26/04/2025",
+    status: "Acumulado: R$ 1.5 milhões"
+  },
+  {
+    nome: "Lotomania",
+    icone: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/3c/Lotomania_logo.png/320px-Lotomania_logo.png",
+    dezenas: ["02", "05", "09", "11", "14", "17", "20", "22", "25", "28", "32", "35", "37", "41", "44", "47", "50", "53", "58", "60"],
+    proximo: "28/04/2025",
+    status: "Sem ganhador"
+  },
+  {
+    nome: "Timemania",
+    icone: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/3a/Timemania_logo.png/320px-Timemania_logo.png",
+    dezenas: ["03", "06", "12", "18", "21", "27", "33"],
+    proximo: "29/04/2025",
+    status: "Acumulado: R$ 3 milhões"
+  },
+  {
+    nome: "Dupla Sena",
+    icone: "https://upload.wikimedia.org/wikipedia/pt/thumb/e/e0/Dupla_Sena_logo.png/320px-Dupla_Sena_logo.png",
+    dezenas: ["07", "14", "22", "30", "38", "45"],
+    proximo: "30/04/2025",
+    status: "Teve ganhador"
+  }
+];
+
+

27c79542fa306174c8b8fc52e3c91458b6413ea9
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-10-17 17:34:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-const cacheName = 'macpoint-cache-v1';
+const cacheName = 'macpoint-cache-v4';
 const filesToCache = [
-  './',
   './index.html',
-  './index1.html',
-  './index2.html',
-  './index3.html',
-  './macpoint.png'
+  './macmac.html',
+  './macpoint-principal.html'
 ];
 
 self.addEventListener('install', e => {
@@ -14,8 +11,22 @@ self.addEventListener('install', e => {
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))
+    )
   );
 });
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        caches.open(cacheName).then(cache => cache.put(e.request, response.clone()));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
